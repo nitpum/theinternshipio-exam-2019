85c8a1ccdeb196bce3a38e9782c493d128b76e82
nitpum <nitpum@outlook.com>
2019-01-30 16:52:04 +0700

    Change output file format and print

--- weather/main.py
+++ weather/main.py
@@ -17,6 +17,8 @@ else:
   root = tree.getroot()
   data = dumps(yahoo.data(root)["current"], indent=4) # Convert to json
   # Write file
-  file = open(filename + ".json", "w")
+  filename = filename.rsplit(".", maxsplit = 1)[0] + ".json"
+  file = open(filename, "w")
   file.write(data)
   file.close()
+  print(filename)
